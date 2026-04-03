fdd675be384c992541fb4814ff28014e14141322
luc <luc@lucemans.nl>
2026-04-03 22:57:06 +0200

    update swap widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,9 +6,22 @@ export const App = () => (
         <div class="flex gap-4">
             <section class="w-[400px] space-y-2">
                 <h2 class="px-2">Create Offer</h2>
-                <div class="card p-4">
-                    <input placeholder="0" />
-                    <input placeholder="0" />
+                <div class="card p-4 space-y-2">
+                    <div>
+                        <label for="input_amount" class="text-sm">
+                            Sell
+                        </label>
+                        <input placeholder="0" class="input w-full" id="input_amount" />
+                    </div>
+                    <div>
+                        <label for="output_amount" class="text-sm">
+                            Buy
+                        </label>
+                        <input placeholder="0" class="input w-full" id="output_amount" />
+                    </div>
+                    <button class="btn-primary w-full">
+                        Create Order
+                    </button>
                 </div>
             </section>
             <section class="grow space-y-2">
