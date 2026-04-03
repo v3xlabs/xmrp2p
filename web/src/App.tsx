d805c473a5174b6f440a5a52bf49705d1f70152d
luc <luc@lucemans.nl>
2026-04-03 20:44:10 +0200

    update web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,12 +6,23 @@ export const App = () => {
     const [count, setCount] = createSignal(0);
 
     return (
-        <>
+        <div class="w-full max-w-7xl mx-auto space-y-4">
             <Navbar />
-            <section>
-                hello
-            </section>
-        </>
+            <div class="flex gap-4">
+                <section class="w-[400px] space-y-2">
+                    <h2 class="px-2">Create Offer</h2>
+                    <div class="card p-4">
+                        hi
+                    </div>
+                </section>
+                <section class="grow space-y-2">
+                    <h2 class="px-2">Your orders</h2>
+                    <div class="card p-4">
+                        hi
+                    </div>
+                </section>
+            </div>
+        </div>
     );
 };
 
--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,7 +1,10 @@
 export const Navbar = () => (
-    <nav>
+    <nav class="w-full flex justify-between px-2 pt-4 pb-2">
+        <div class="font-bold text-xl">
+            xmrp2p.eth
+        </div>
         <div>
-            hello
+            <button class="btn-primary">Connect</button>
         </div>
     </nav>
 );
