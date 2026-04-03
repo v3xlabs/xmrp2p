1ab445d35af9fea1696633668868f70a861afb8c
v1rtl <hi@v1rtl.site>
2026-04-03 22:40:26 +0200

    reintroduce types

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,6 +1,2 @@
-import type { Address, Provider } from "ox";
-
-export type ContractCall<T = object> = {
-    provider: Provider.Provider;
-    contractAddress: Address.Address;
-} & T;
+export * from "./wallet/createBuyOffer";
+export * from "./wallet/createSellOffer";
