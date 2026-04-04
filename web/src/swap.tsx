c2047d0550e5bbf47d8877d0ec9c734d4be3d811
v1rtl <hi@v1rtl.site>
2026-04-04 20:35:53 +0300

    fix swap: auto-swap tokens when selecting same asset

--- web/src/swap.tsx
+++ web/src/swap.tsx
@@ -68,7 +68,7 @@ export const Swap = () => {
           <label for="input_amount" class="text-md py-1 flex justify-between items-center gap-1">
             <span>Sell</span>
           </label>
-          <TokenSelector token={fromToken} setToken={t => t !== toToken() && setFromToken(t)} />
+<TokenSelector token={fromToken} setToken={t => t === toToken() ? (setToToken(fromToken()), setFromToken(t)) : setFromToken(t)} />
         </div>
         <input placeholder="0" class="input w-full" id="input_amount" />
       </div>
@@ -91,7 +91,7 @@ export const Swap = () => {
           <label for="output_amount" class="text-md py-1 gap-1">
             <span>Buy</span>
           </label>
-          <TokenSelector token={toToken} setToken={t => t !== fromToken() && setToToken(t)} />
+<TokenSelector token={toToken} setToken={t => t === fromToken() ? (setFromToken(toToken()), setToToken(t)) : setToToken(t)} />
         </div>
         <input placeholder="0" class="input w-full" id="output_amount" />
       </div>
