152f913a2cb26ea1ed7eded55ea6791bd407d3c5
v1rtl <hi@v1rtl.site>
2026-04-04 14:10:50 +0300

    export everything in index.ts

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,5 +2,33 @@ export {
     deriveEvmAccountFromRootSeed,
     deriveMoneroHotWalletSeed,
 } from "./keyDerivation";
+export * from "./public/getBuyOffer";
+export * from "./public/getFundingRequest";
+export * from "./public/getLiability";
+export * from "./public/getParameters";
+export * from "./public/getSellOffer";
+export * from "./public/getXMRPrice";
+export * from "./public/isKeyUsed";
+export * from "./public/listBuyOffers";
+export * from "./public/listFundingRequests";
+export * from "./public/listSellOffers";
+export * from "./wallet/cancelBuyOffer";
+export * from "./wallet/cancelFundingRequest";
+export * from "./wallet/cancelSellOffer";
+export * from "./wallet/claim";
+export * from "./wallet/claimDeposit";
+export * from "./wallet/claimFundingRequest";
 export * from "./wallet/createBuyOffer";
+export * from "./wallet/createFundingRequest";
 export * from "./wallet/createSellOffer";
+export * from "./wallet/defundFundingRequest";
+export * from "./wallet/fundFundingRequest";
+export * from "./wallet/message";
+export * from "./wallet/ready";
+export * from "./wallet/refund";
+export * from "./wallet/setParameters";
+export * from "./wallet/setPriceOracle";
+export * from "./wallet/takeBuyOffer";
+export * from "./wallet/takeSellOffer";
+export * from "./wallet/updateBuyOffer";
+export * from "./wallet/updateSellOffer";
